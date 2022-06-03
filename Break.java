e1b75659845c4016e22d9c337e7fed0dbff35690
Neel0908 <neelbhanderi54992@gmail.com>
2022-06-03 09:58:26 +0530

    Commit for add Break.java file

---
+++ Break.java
@@ -0,0 +1,29 @@
+package corejava;
+
+/**
+ * This program is for
+ * print 1 to 10 number
+ * and skip any one after 7
+ * @author macbook
+ *
+ */
+public class Break {
+
+	/*
+	 * This method is for
+	 * print 1 to 10 number
+	 * and skip any one after 7
+	 * using break/continue
+	 */
+	public static void main(String[] args) {
+		 for (int i = 1; i <= 10; i++) {
+
+			if (i == 8) { // If for (i <= 8)
+				continue;
+			}
+
+			System.out.println(+ i);
+		}
+	}
+
+}
