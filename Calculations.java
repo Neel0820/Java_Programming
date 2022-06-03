3f545ae1ea38be0b71d46b7490bf9c10dd1bfb6b
Neel0908 <neelbhanderi54992@gmail.com>
2022-06-03 10:00:02 +0530

    Commit foe add Calcutation.java file

---
+++ Calculations.java
@@ -0,0 +1,44 @@
+package corejava;
+
+/**
+ * This program is for
+ * find sum of two numbers
+ * @author macbook
+ */
+public class Calculations {
+
+	int firstNumber = 32;
+	int secondNumber = 12;
+
+	/*
+	 * This method is for
+	 * Find subtraction of two numbers
+	 * using constructor
+	 */
+	public Calculations() {
+
+		// this variable is for store the value of addition of both number
+		int subtraction = firstNumber - secondNumber;
+		System.out.println("Subtraction of both number is : " + subtraction);
+	}
+
+	/*
+	 * This method is for
+	 * find addition of two numbers
+	 * using parameterized constructor
+	 */
+	public Calculations(int firstNumber, int secondNumber) {
+
+		// this variable is for store the value of addition of both number
+		int addition = firstNumber + secondNumber;
+		System.out.println("Addition of both number is : " + addition);
+	}
+
+	/*
+	 * This is the main method uses of constructor method.
+	 */
+	public static void main(String[] args) {
+		Calculations calculation = new Calculations(20, 12);
+	}
+
+}
